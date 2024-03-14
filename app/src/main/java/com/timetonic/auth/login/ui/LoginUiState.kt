c7c19256c057082c024f092807c26a3f786ef844
Gustavo <gusreyesdev@gmail.com>
2024-03-13 19:00:10 -0500

    State UI

--- app/src/main/java/com/timetonic/auth/login/ui/LoginUiState.kt
+++ app/src/main/java/com/timetonic/auth/login/ui/LoginUiState.kt
@@ -1,11 +1,7 @@
 package com.timetonic.auth.login.ui
 
 sealed class LoginUiState<T> {
-
     class Loading<T> : LoginUiState<T>()
-
     data class Success<T>(val data: T) : LoginUiState<T>()
-
     data class Error<T>(val error: T) : LoginUiState<T>()
-
 }
